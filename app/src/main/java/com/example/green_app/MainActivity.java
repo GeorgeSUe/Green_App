738b5b6ce99f8729b6f0c33826294b97ebe8c9dd
GeorgeSUe <oopsu425@gmail.com>
2020-04-15 03:26:52 +0800

    Initial button
    1. Mainpage button "Start" link to the Homepage Activity

--- app/src/main/java/com/example/green_app/MainActivity.java
+++ app/src/main/java/com/example/green_app/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.green_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.start);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                openHomepageActivity();
+            }
+        });
+
+
     }
+
+    private void openHomepageActivity() {
+        Intent intent = new Intent(this, HomepageActivity.class);
+        startActivity(intent);
+    }
+
+
 }
+
+
