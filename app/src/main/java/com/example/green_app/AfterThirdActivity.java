8b975ae893fcebbf41e5e27532906cd68e408128
GeorgeSUe <oopsu425@gmail.com>
2020-04-17 02:57:58 +0800

    Google Map Feature add

--- app/src/main/java/com/example/green_app/AfterThirdActivity.java
+++ app/src/main/java/com/example/green_app/AfterThirdActivity.java
@@ -2,13 +2,31 @@ package com.example.green_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AfterThirdActivity extends AppCompatActivity {
+    private Button map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_third_activity);
+
+        map = (Button) findViewById(R.id.mapbutton);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmapsactivity();
+            }
+        });
     }
+
+    private void openmapsactivity() {
+        Intent mapsactivity = new Intent(this, MapsActivity.class);
+        startActivity(mapsactivity);
+    }
+
 }
