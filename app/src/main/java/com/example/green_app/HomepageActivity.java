0a76ac73ebbcfce75cafa2a552d8c2f918b5b9d5
GeorgeSUe <45215494+GeorgeSUe@users.noreply.github.com>
2020-04-19 03:45:32 +0800

    adjust layout

--- app/src/main/java/com/example/green_app/HomepageActivity.java
+++ app/src/main/java/com/example/green_app/HomepageActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomepageActivity extends AppCompatActivity {
     private Button buttonbusiness,buttonconsumer,buttontrending;
@@ -39,6 +40,9 @@ public class HomepageActivity extends AppCompatActivity {
                 opentrending();
             }
         });
+
+
+
     }
 
     private void opentrending() {
